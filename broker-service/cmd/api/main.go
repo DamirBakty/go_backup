b5deac99a71e0470b98381a2908a085d798a5932
damir <damir@mail.ru>
2024-01-11 18:55:00 +0600

    backup

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "8000"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+	log.Printf("Starting broker-service service on poer %s", webPort)
+
+	// define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start Server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
